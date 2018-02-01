4079f0642ff889a0df6d19c7419ac875823121ab
Max Llewellyn <max.llewellyn@gmail.com>
2018-01-31 19:46:34 -0500

    Forgot to add new files.

---
+++ src/org/usfirst/frc/team2791/robot/commands/drivetrain/SetDrivetrainShifterMode.java
@@ -0,0 +1,42 @@
+package org.usfirst.frc.team2791.robot.commands.drivetrain;
+
+import org.usfirst.frc.team2791.robot.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class SetDrivetrainShifterMode extends Command {
+	private boolean shiftToDrive;
+
+    public SetDrivetrainShifterMode(boolean shiftToDrive) {
+//    	requires(Robot.drivetrain);
+	// IMPORTANT NOTE!! WE DO NOT REQUIRE DRIVE TRAIN HERE BECAUSE WE DO NOT WANT TO STOP THE DriveWithJoystick
+	// Command. when this is running. Maybe Max S was right about the command based framework.
+    	this.shiftToDrive = shiftToDrive;
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    	Robot.drivetrain.setDriveOrRampMode(shiftToDrive);
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return false;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    }
+}
