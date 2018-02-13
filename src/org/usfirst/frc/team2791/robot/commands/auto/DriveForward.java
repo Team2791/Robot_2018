2f711d83646e5b58418ad69093f5bf34f77c6ae9
John <johnyin596@gmail.com>
2018-02-13 01:22:56 +0000

    made Drive acroos the line command

--- src/org/usfirst/frc/team2791/robot/commands/auto/DriveForward.java
+++ src/org/usfirst/frc/team2791/robot/commands/auto/DriveForward.java
@@ -1,16 +1,32 @@
 package org.usfirst.frc.team2791.robot.commands.auto;
 
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import edu.wpi.first.wpilibj.Talon;
+import edu.wpi.first.wpilibj.command.Subsystem;
 import org.usfirst.frc.team2791.robot.Robot;
 
 import edu.wpi.first.wpilibj.command.Command;
 
+import static org.usfirst.frc.team2791.robot.Robot.drivetrain;
+import static org.usfirst.frc.team2791.robot.util.Constants.LINE_DISTANCE;
+import static org.usfirst.frc.team2791.robot.util.Constants.SMALL_DISTANCE;
+
 public class DriveForward extends Command {
+    private double speed;
     public DriveForward(){
-        requires(Robot.drivetrain);
+        requires(drivetrain);
     }
-    
+    public void intialize(){
+}
     public void execute() {
-    	// TODO PUT SOME CODE HERE
+        if (LINE_DISTANCE > Robot.drivetrain.getAverageDist()) {
+            speed = .3;
+        } else if (SMALL_DISTANCE < LINE_DISTANCE && LINE_DISTANCE > Robot.drivetrain.getAverageDist()) {
+            speed = .1;
+        } else if (LINE_DISTANCE == Robot.drivetrain.getAverageDist()) {
+            speed = 0;
+        }
+        Robot.drivetrain.setLeftRightMotorOutputs(speed, speed);
     }
 
     @Override
