4ee5d7c85486ccd1d444763c72e47d2b0029633a
Max Llewellyn <max.llewellyn@gmail.com>
2018-02-18 16:36:33 -0500

    didn't add a file.

---
+++ src/org/usfirst/frc/team2791/robot/commands/auto/BangBangTurnSwitchLEFT.java
@@ -0,0 +1,51 @@
+package org.usfirst.frc.team2791.robot.commands.auto;
+
+import org.usfirst.frc.team2791.robot.Constants;
+import org.usfirst.frc.team2791.robot.commands.auto.bangbang.DriveEncoderBangBang;
+import org.usfirst.frc.team2791.robot.commands.auto.bangbang.TurnGyroBangBang;
+import org.usfirst.frc.team2791.robot.commands.lift.GoToHeight;
+import org.usfirst.frc.team2791.robot.commands.manipulator.SetManipulatorRetracted;
+import org.usfirst.frc.team2791.robot.commands.manipulator.ShootCube;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+/**
+ *
+ */
+public class BangBangTurnSwitchLEFT extends CommandGroup {
+
+    public BangBangTurnSwitchLEFT() {
+        // Add Commands here:
+        // e.g. addSequential(new Command1());
+        //      addSequential(new Command2());
+        // these will run in order.
+
+        // To run multiple commands at the same time,
+        // use addParallel()
+        // e.g. addParallel(new Command1());
+        //      addSequential(new Command2());
+        // Command1 and Command2 will run in parallel.
+
+        // A command group will require all of the subsystems that each member
+        // would require.
+        // e.g. if Command1 requires chassis, and Command2 requires arm,
+        // a CommandGroup containing them would require both the chassis and the
+        // arm.
+    	
+    	// drive off the wall
+    	// 3rd arg is drive distance 
+    	addSequential(new DriveEncoderBangBang(0.4, 0,  24, 100));
+    	addParallel(new SetManipulatorRetracted(true));
+    	// turn towards the left side
+    	addSequential(new TurnGyroBangBang(-30, -0.3, 100));
+    	// drive towards the left side
+    	addSequential(new DriveEncoderBangBang(0.4, 0,  36, 100));
+    	addParallel(new GoToHeight(8)); // was 13
+    	// turn to face the switch
+    	addSequential(new TurnGyroBangBang(30, 0.3, 100));
+    	// drive into the switch. Low power so we'll hit the wall and use the timeout to stop
+    	addSequential(new DriveEncoderBangBang(0.25, 0,  24, 3));
+    	// score
+    	addSequential(new ShootCube(Constants.LARGE_OUTPUT_SPEED));
+    }
+}
