29c7ac5e5880ffdc6561bff7931aed32c064115c
Max Llewellyn <max.llewellyn@gmail.com>
2018-02-18 21:43:16 -0500

    Have a reasonable working right side switch auto.

--- src/org/usfirst/frc/team2791/robot/commands/auto/BangBangTurnSwitchRIGHT.java
+++ src/org/usfirst/frc/team2791/robot/commands/auto/BangBangTurnSwitchRIGHT.java
@@ -1,8 +1,8 @@
 package org.usfirst.frc.team2791.robot.commands.auto;
 
 import org.usfirst.frc.team2791.robot.Constants;
-import org.usfirst.frc.team2791.robot.commands.auto.bangbang.DriveEncoderBangBang;
 import org.usfirst.frc.team2791.robot.commands.auto.bangbang.TurnGyroBangBang;
+import org.usfirst.frc.team2791.robot.commands.auto.pid.DriveEncoderBangBangGyroPID;
 import org.usfirst.frc.team2791.robot.commands.lift.GoToHeight;
 import org.usfirst.frc.team2791.robot.commands.manipulator.SetManipulatorRetracted;
 import org.usfirst.frc.team2791.robot.commands.manipulator.ShootCube;
@@ -37,17 +37,19 @@ public class BangBangTurnSwitchRIGHT extends CommandGroup {
     	
     	// NOTE. ALl bang bang distances have a little removed to compensate for overshoot.
 
-    	addSequential(new DriveEncoderBangBang(0.3, 0,  10-2, 100));
+    	addSequential(new DriveEncoderBangBangGyroPID(0.3, 5-2, 100));
     	addParallel(new SetManipulatorRetracted(true));
     	// turn towards the left side
-    	addSequential(new TurnGyroBangBang((45-5), 0.3, 100));
+    	addSequential(new TurnGyroBangBang(60, 0.3, 100));
     	// drive towards the left side
     	addParallel(new GoToHeight(8));
-    	addSequential(new DriveEncoderBangBang(0.3, 0,  22.6-2, 100));
+    	
+    	addSequential(new DriveEncoderBangBangGyroPID(0.3, 13.9, 100));
     	// turn to face the switch
-    	addSequential(new TurnGyroBangBang(-(45-5), -0.3, 100));
+    	addSequential(new TurnGyroBangBang(-60, -0.3, 100));
     	// drive into the switch. Low power so we'll hit the wall and use the timeout to stop
-    	addSequential(new DriveEncoderBangBang(0.2, 0,  8, 3));
+    	addSequential(new DriveEncoderBangBangGyroPID(0.4, 14, 3.5));
+    	addSequential(new DriveEncoderBangBangGyroPID(0.2, 14, 3.5));
     	// score
     	addSequential(new ShootCube(Constants.LARGE_OUTPUT_SPEED));
     }
