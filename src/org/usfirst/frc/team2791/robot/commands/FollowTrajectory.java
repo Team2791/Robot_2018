4fa96d4cf7accacbc38c71a48418bd0dec8b4c52
John <johnyin596@gmail.com>
2018-03-30 20:17:48 +0100

    finished follow trajectory based on team 319 still a few errors
    a few errors, I'm going to try and fix and reshape the code according to the manual and what team 319 did

--- src/org/usfirst/frc/team2791/robot/commands/FollowTrajectory.java
+++ src/org/usfirst/frc/team2791/robot/commands/FollowTrajectory.java
@@ -1,32 +1,24 @@
 package org.usfirst.frc.team2791.robot.commands;
 import com.ctre.phoenix.motion.MotionProfileStatus;
-import edu.wpi.first.wpilibj.command.Command;
-import jaci.pathfinder.Trajectory;
-import org.usfirst.frc.team2791.robot.Constants;
-import org.usfirst.frc.team2791.robot.Robot;
-import org.usfirst.frc.team2791.robot.RobotMap;
-import org.usfirst.frc.team2791.robot.commands.auto.ShakerSrx;
-import org.usfirst.frc.team2791.robot.commands.lift.StopLift;
-import com.ctre.phoenix.motion.SetValueMotionProfile;
-import com.ctre.phoenix.motion.TrajectoryPoint;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
-import com.ctre.phoenix.motorcontrol.can.VictorSPX;
-import com.ctre.phoenix.motorcontrol.can.BaseMotorController;
-import com.ctre.phoenix.motorcontrol.NeutralMode;
-import org.usfirst.frc.team2791.robot.subsystems.ShakerLift;
-import org.usfirst.frc.team2791.robot.subsystems.ShakerDrivetrain;
+import edu.wpi.first.wpilibj.command.Command;
+import org.usfirst.frc.team2791.robot.Constants;
+import org.usfirst.frc.team2791.robot.Robot;
+import org.usfirst.frc.team2791.robot.commands.auto.ShakerSrx;
+import com.ctre.phoenix.motion.SetValueMotionProfile;
+import com.ctre.phoenix.motion.TrajectoryPoint;
 import org.usfirst.frc.team2791.robot.util.SrxTrajectory;
 import edu.wpi.first.wpilibj.Notifier;
 import org.usfirst.frc.team2791.robot.util.SrxMotionProfile;
 
 public class FollowTrajectory extends Command {
+    private String trajectoryName = "";
+    private SrxTrajectory trajectoryToFollow = null;
     private ShakerSrx leftTalon = Robot.drivetrain.leftDrive;
     private ShakerSrx rightTalon = Robot.drivetrain.rightDrive;
-    private String tragectoryName = "";
     private int kMinPointsInTalon = 5;
 
-    private SrxTrajectory trajectorytoFollow = null;
 
     private MotionProfileStatus rightStatus = new MotionProfileStatus();
     private MotionProfileStatus leftStatus = new MotionProfileStatus();
@@ -35,12 +27,29 @@ public class FollowTrajectory extends Command {
 
     private SetValueMotionProfile setValue = SetValueMotionProfile.Disable;
 
+    public FollowTrajectory(String trajectoryName) {
+        requires(Robot.drivetrain);
+        this.trajectoryName = trajectoryName;
+    }
+
+    public FollowTrajectory(SrxTrajectory trajectoryToFollow) {
+        requires(Robot.drivetrain);
+        this.trajectoryToFollow = trajectoryToFollow;
+    }
+    @Override
+    protected boolean isFinished() {
+        return false;
+    }
+
     private class BufferLoader implements java.lang.Runnable {
-        private final String trajectoryName;
+        private String trajectoryName;
         private int leftLastPointSent = 0;
         private int rightLastPointSent = 0;
+        private boolean hasPathStarted;
+        private SrxTrajectory trajectoryToFollow;
 
-        public BufferLoader() {
+        public BufferLoader(String trajectoryName) {
+            this.trajectoryName = trajectoryName;
             this.leftLastPointSent = 0;
             this.rightLastPointSent = 0;
         }
@@ -49,8 +58,8 @@ public class FollowTrajectory extends Command {
         public void run() {
             leftTalon.processMotionProfileBuffer();
             rightTalon.processMotionProfileBuffer();
-            leftLastPointSent = manageBuffer(leftTalon, trajectorytoFollow.leftProfile, Robot.drivetrain.getGyroRate(), leftLastPointSent);
-            rightLastPointSent = manageBuffer(rightTalon, trajectorytoFollow.rightProfile, Robot.drivetrain.getGyroRate(), rightLastPointSent);
+            leftLastPointSent = manageBuffer(leftTalon, trajectoryToFollow.leftProfile, 2, leftLastPointSent);
+            rightLastPointSent = manageBuffer(rightTalon, trajectoryToFollow.rightProfile, 2, rightLastPointSent);
         }
 
         private int manageBuffer(ShakerSrx talon, SrxMotionProfile prof, int pidfslot, int lastPointSent) {
@@ -77,22 +86,101 @@ public class FollowTrajectory extends Command {
             return lastPointSent;
         }
 private TrajectoryPoint.TrajectoryDuration getTrajectoryDuration(int ms){
-    TrajectoryPoint.TrajectoryDuration retval = TrajectoryPoint.TrajectoryDuration.Trajectory_Duration_0ms.TrajectoryDuration
+   TrajectoryPoint.TrajectoryDuration retval = TrajectoryPoint.TrajectoryDuration.Trajectory_Duration_0ms;
 
     retval= retval.valueOf(ms);
     return retval;
 }
-private Notifier bufferNotifier;
+        private Notifier bufferNotifier;
 
-        public FollowTrajectory(String trajectoryName){
-            this.trajectoryName = trajectoryName;
-            requires(Robot.drivetrain);
+        // Called just before this Command runs the first time
+        protected void initialize() {
+            this.hasPathStarted = false;
+            setUpTalon(rightTalon);
+            setUpTalon(leftTalon);
 
+            setValue = SetValueMotionProfile.Disable;
+
+            leftTalon.set(ControlMode.MotionProfile, setValue.value);
+            rightTalon.set(ControlMode.MotionProfile, setValue.value);
+
+            bufferNotifier = new Notifier(new BufferLoader());
+            bufferNotifier.startPeriodic(.005);
         }
 
-        @Override
+        // Called repeatedly when this Command is scheduled to run
+        protected void execute() {
+
+            rightTalon.getMotionProfileStatus(rightStatus);
+            leftTalon.getMotionProfileStatus(leftStatus);
+
+            if (rightStatus.isUnderrun || leftStatus.isUnderrun) {
+                // if either MP has underrun, stop both
+                System.out.println("Motion profile has underrun!");
+                setValue = SetValueMotionProfile.Disable;
+            } else if (rightStatus.btmBufferCnt > kMinPointsInTalon && leftStatus.btmBufferCnt > kMinPointsInTalon) {
+                // if we have enough points in the talon, go.
+                setValue = SetValueMotionProfile.Enable;
+                hasPathStarted = true;
+            } else if (rightStatus.activePointValid && rightStatus.isLast && leftStatus.activePointValid
+                    && leftStatus.isLast) {
+                // if both profiles are at their last points, hold the last point
+                setValue = SetValueMotionProfile.Hold;
+                // System.out.println("HOLDING LAST POINT!!!!!");
+            }
+
+            leftTalon.set(ControlMode.MotionProfile, setValue.value);
+            rightTalon.set(ControlMode.MotionProfile, setValue.value);
+        }
+
+        // Make this return true when this Command no longer needs to run execute()
         protected boolean isFinished() {
-            return false;
+            if (!hasPathStarted) {
+                return false;
+            }
+            boolean leftComplete = leftStatus.activePointValid && leftStatus.isLast;
+            boolean rightComplete = rightStatus.activePointValid && rightStatus.isLast;
+            // System.out.println("Complete: " + leftComplete + "," + rightComplete);
+            boolean trajectoryComplete = leftComplete && rightComplete;
+            if (trajectoryComplete) {
+                System.out.println("Finished Trajectory");
+            }
+            boolean isFinished = false;
+            return trajectoryComplete || isFinished;
         }
+
+        // Called once after isFinished returns true
+        protected void end() {
+            cleanUp();
+        }
+
+        // Called when another command which requires one or more of the same
+        // subsystems is scheduled to run
+        protected void interrupted() {
+            cleanUp();
+        }
+
+        public void cleanUp() {
+            resetTalon(rightTalon, ControlMode.PercentOutput, 0);
+            resetTalon(leftTalon, ControlMode.PercentOutput, 0);
+            bufferNotifier.stop();
+        }
+
+        // set up the talon for motion profile control
+        public void setUpTalon(TalonSRX talon) {
+            talon.clearMotionProfileTrajectories();
+            talon.changeMotionControlFramePeriod(5);
+        }
+
+        // set the to the desired controlMode
+        // used at the end of the motion profile
+        public void resetTalon(TalonSRX talon, ControlMode controlMode, double setValue) {
+            // System.out.println("Clearing MP Trajectories");
+            talon.clearMotionProfileTrajectories();
+            talon.set(ControlMode.MotionProfile, SetValueMotionProfile.Disable.value);
+            talon.set(controlMode, setValue);
+        }
+
+
     }
 }
