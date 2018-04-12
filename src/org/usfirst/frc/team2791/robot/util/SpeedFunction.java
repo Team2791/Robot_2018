2cfb8ec344c4629323a4e01a48ed545dc7dcf844
John <johnyin596@gmail.com>
2018-04-12 19:53:12 -0400

    .

--- src/org/usfirst/frc/team2791/robot/util/SpeedFunction.java
+++ src/org/usfirst/frc/team2791/robot/util/SpeedFunction.java
@@ -3,8 +3,6 @@ package org.usfirst.frc.team2791.robot.util;
 import java.lang.reflect.Array;
 
 public class SpeedFunction {
-    public SpeedFunction([[0, 2], [0.3,0.75], [0.75, .3]]){
-    }
     public double getSpeed(double distanceTraveled){
         if (distanceTraveled < 0.25 ){
             return .4;
